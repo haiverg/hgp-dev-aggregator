f04fd0ba23ff0f11e4163370c5afed59e7cfe202
haiverg <haiver.galeano@gmail.com>
2016-09-15 19:55:16 -0500

    Ready to test

---
+++ hgp-dev-aggregator/src/main/java/com/redhat/training/BodyAggregationStrategy.java
@@ -0,0 +1,20 @@
+package com.redhat.training;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+public class BodyAggregationStrategy implements AggregationStrategy {
+
+	@Override
+	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+		if (oldExchange == null) {
+			return newExchange;
+		}
+		String newBody = newExchange.getIn().getBody(String.class);
+		String oldBody = newExchange.getIn().getBody(String.class);
+		newBody = newBody.concat(oldBody);
+		newExchange.getIn().setBody(newBody);		
+		return newExchange;
+	}
+
+}
--- hgp-dev-aggregator/src/main/java/com/redhat/training/JavaRouteBuilder.java
+++ hgp-dev-aggregator/src/main/java/com/redhat/training/JavaRouteBuilder.java
@@ -1,19 +1,18 @@
 package com.redhat.training;
 
-import javax.print.attribute.standard.Destination;
-
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
 
 public class JavaRouteBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-
+		AggregationStrategy strategy = new BodyAggregationStrategy();
 		from("file:in?noop=true")
 		.process(new DestinationProcessor())
+		.aggregate(header("customer"),strategy)
+		.completionSize(5)
 		.to("file:out");
-			
-
 	}
 
 }
